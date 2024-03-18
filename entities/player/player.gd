be65cc8a2e435ca597f61e26c609d6d35a642c4a
jonael-garcia26 <jonael.garcia@upr.edu>
2024-03-18 17:28:37 -0400

    player and camera movement
    Cree una plataforma para test al personaje o tests en general, termine de hacer el movimiento del personaje al igual que la camara primera persona

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,9 +3,31 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var pause = false
+@export var sensitivity = .002
+@onready var neck := $Neck
+@onready var camera := $Neck/Camera3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"): #Delete later, debugg purpose only
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	#if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+	if event is InputEventMouseMotion:
+		var relative_x = event.relative.x
+		var relative_y = event.relative.y
+		
+		neck.rotate_y(-relative_x * sensitivity)
+		camera.rotate_x(-relative_y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 
 
 func _physics_process(delta):
@@ -19,13 +41,15 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
 
+	
 	move_and_slide()
