3077a88ffc690e1e25a70d612b854d0880c5558e
Gabriel <gabriel.viera4@upr.edu>
2024-04-02 17:35:46 -0400

    Object to be found

---
+++ world/Consume/object_to_be_found.gd
@@ -0,0 +1,17 @@
+class_name findable
+extends StaticBody3D
+
+var particle : GPUParticles3D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#particle = load()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func consume():
+	pass	
