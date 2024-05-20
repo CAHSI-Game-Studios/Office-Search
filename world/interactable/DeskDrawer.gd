0e45cbc1ceb23b9a2083b63222a887e6b8c39b73
Gabriel <gabriel.viera4@upr.edu>
2024-05-20 14:10:59 -0400

    Wall Drawer Animation

--- world/interactable/DeskDrawer.gd
+++ world/interactable/DeskDrawer.gd
@@ -1,10 +1,15 @@
 class_name Drawer
-extends Node3D
+extends StaticBody3D
 
 @export var animation : AnimationPlayer
 
 var drawer_state = true
-
+func _ready():
+	set_collision_layer_value(1,true)
+	set_collision_layer_value(2,true)
+	
+	set_collision_mask_value(1,true)
+	
 func drawer_interaction():
 	
 	if Input.is_action_just_pressed("l_click") and !animation.is_playing():
