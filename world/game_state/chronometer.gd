1ae9eeaad94606960918922365d02440f96f1ff6
Gabriel <gabriel.viera4@upr.edu>
2024-08-21 00:33:44 -0400

    Add Game Over State

--- world/game_state/chronometer.gd
+++ world/game_state/chronometer.gd
@@ -17,3 +17,6 @@ func _process(delta):
 	
 func activate_timer():
 	timer_on = true
+
+func desactive_timer():
+	timer_on = false
--- world/game_state/game_state.gd
+++ world/game_state/game_state.gd
@@ -7,9 +7,12 @@ extends Node3D
 var findables_list:Array = []
 
 func _ready():
+	PlayerTime.time = 0
+	
 	set_up_findable_objects()
 	chronometer.activate_timer()
 	check_list.checklist_setup(findables_list)
+	
 
 func set_up_findable_objects():
 	for child_index in findable_objects.get_child_count():
@@ -18,3 +21,13 @@ func set_up_findable_objects():
 # The Findable Object will emit a signal before being consumed. 
 func _on_findable_object_consumed(name):
 	check_list.check_item(name)
+	if(findables_list.size() > 0):
+		findables_list.remove_at(findables_list.find(name)) 
+	is_game_over()
+
+func is_game_over():
+	if (findables_list.size() == 0):
+			chronometer.desactive_timer()
+			PlayerTime.time = chronometer.time
+			await get_tree().create_timer(4.0).timeout
+			get_tree().change_scene_to_file("res://menus/game_over_screen.tscn")
