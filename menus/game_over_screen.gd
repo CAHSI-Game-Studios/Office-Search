2687a3b655cdf809ecfb7a2b40ddc07e83f95805
Gabriel <gabriel.viera4@upr.edu>
2024-08-27 20:58:38 -0400

    Add Credits Screen

--- menus/game_over_screen.gd
+++ menus/game_over_screen.gd
@@ -1,12 +1,19 @@
 extends Control
 
-@onready var label_container : VBoxContainer = $VBoxContainer/MarginContainer/VBoxContainer
+@onready var label : Label = $VBoxContainer/MarginContainer/Label
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	label.text = "Total: " + str("%10.2f"%PlayerData.total_time) + "s"
+	
+	
 	var keys: Array = PlayerData.map_of_times.keys()
 	keys.sort()
+	
+	# Create data directory
 	DirAccess.make_dir_absolute("user://data")
+	
 	var file_name = "user://data/" + Time.get_datetime_string_from_system()
 	print(file_name)
 	var file = FileAccess.open(file_name, FileAccess.WRITE)
@@ -21,9 +28,6 @@ func _on_play_again_pressed():
 		PlayerData.map_of_times = {}
 
 func create_new_time_label(time, text):
-	var label = Label.new()
 	var full_text = text + ": " + str("%10.2f"%time) + "s"
-	label.text = full_text
-	label_container.add_child(label)
 	return full_text
 	
